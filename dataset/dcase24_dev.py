4eb122a8278df361d6d34e638d261ba0af02fdd8
seanyeo300 <seanyeo300@gmail.com>
2024-04-17 18:42:34 +0800

    run_training modified to use dcase24_dev.py
    Will need to fork the run_training script for logit computation

--- dataset/dcase24_dev.py
+++ dataset/dcase24_dev.py
@@ -20,8 +20,8 @@ dataset_config = {
     "split_url": "https://github.com/CPJKU/dcase2024_task1_baseline/releases/download/files/",
     "test_split_csv": "test.csv",
     "eval_dir": os.path.join(dataset_dir), 
-    "eval_meta_csv": os.path.join(dataset_dir, "meta.csv"),
-    "logits_file": os.path.join("predictions","1ea864zz", "logits.pt")
+    "eval_meta_csv": os.path.join(dataset_dir, "split5.csv"),
+    "logits_file": os.path.join("predictions","1ea864zz", "logits.pt") #specifies where the logit and predictions are stored. Still need to provide script with ckpt_id
     # "eval_dir": os.path.join(dataset_dir, "TAU-urban-acoustic-scenes-2024-mobile-evaluation"), 
     # "eval_meta_csv": os.path.join(dataset_dir,  "TAU-urban-acoustic-scenes-2024-mobile-evaluation", "meta.csv")
 }
