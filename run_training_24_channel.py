bf17a2db3fe9a7ea6268d796a819294516140a8d
seanyeo300 <seanyeo300@gmail.com>
2024-05-31 13:38:35 +0800

    channel scripts

--- run_training_24_channel.py
+++ run_training_24_channel.py
@@ -468,7 +468,7 @@ if __name__ == '__main__':
 
     # general
     parser.add_argument('--project_name', type=str, default="DCASE24_Task1")
-    parser.add_argument('--experiment_name', type=str, default="Baseline_Ali_sub5_16_channel")
+    parser.add_argument('--experiment_name', type=str, default="Baseline_Ali_sub5_24 _channel")
     parser.add_argument('--num_workers', type=int, default=0)  # number of workers for dataloaders
     parser.add_argument('--precision', type=str, default="32")
 
@@ -485,7 +485,7 @@ if __name__ == '__main__':
     parser.add_argument('--n_classes', type=int, default=10)  # classification model with 'n_classes' output neurons
     parser.add_argument('--in_channels', type=int, default=1)
     # adapt the complexity of the neural network (3 main dimensions to scale the baseline)
-    parser.add_argument('--base_channels', type=int, default=16)
+    parser.add_argument('--base_channels', type=int, default=24)
     parser.add_argument('--channels_multiplier', type=float, default=1.8)
     parser.add_argument('--expansion_rate', type=float, default=2.1)
 
